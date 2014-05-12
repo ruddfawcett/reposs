8adf73773e0d4698506742405a238ae6fa9d75c1
rexfinn <rexcfinn@gmail.com>
2014-05-12 18:52:43 -0400

    Fixed ? to &

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 	
 	curl_setopt_array($curl,
 		array(
-			CURLOPT_URL => "https://api.github.com/repos/".$_GET['path'],
+			CURLOPT_URL => 'https://api.github.com/repos/'.$_GET['path'],
 			CURLOPT_RETURNTRANSFER => 1,
 			CURLOPT_TIMEOUT => '3',
-			CURLOPT_HTTPHEADER => array("User-Agent: reposs [shields.io service]")
+			CURLOPT_HTTPHEADER => array('User-Agent: reposs [shields.io service]')
 		)
 	);
 	
@@ -16,10 +16,15 @@
 	
 	curl_close($curl);
 	
-	if (array_key_exists("color", $_GET)) {
-		$color = $_GET["color"];
+	if (array_key_exists('color', $_GET)) {
+		$color = $_GET['color'];
 	}
-	else $color = "blue";
+	else $color = 'blue';	
+	
+	if (array_key_exists('style', $_GET) && array_key_exists('style', $_GET) == 'flat') {
+		$style = '&style=flat';
+	}
+	else $style = '';
 	
 	// Taken from here: http://stackoverflow.com/a/2510540
 	function formatBytes($size, $precision = 2) {
@@ -29,5 +34,5 @@
 	    return round(pow(1024, $base - floor($base)), $precision) . $suffixes[floor($base)];
 	}
 	
-	echo file_get_contents("http://img.shields.io/badge/repo_size-".str_replace(" ","_",formatBytes($result["size"], 1))."-".$color.".svg");
+	echo file_get_contents('http://img.shields.io/badge/repo_size-'.str_replace(' ','_',formatBytes($result['size'], 1)).'-'.$color.'.svg'.$style);
 ?>
