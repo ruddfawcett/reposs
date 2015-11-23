ba2383eb2542c1d3a60ece33671213e5a98ddc88
Rada Bogdan Raul <raoul_ice@yahoo.com>
2015-11-23 10:18:28 +0200

    curl and github authorization

--- index.php
+++ index.php
@@ -4,18 +4,21 @@
 	header("Cache-Control:no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=-1");
 	header("Pragma: no-cache");
 	header('Content-Type: image/svg+xml; Content-Encoding: gzip; charset=utf-8');
-
+	
+	$fp = fopen(dirname(__FILE__).'/access.log', 'w');
 	$curl = curl_init();
 
 	curl_setopt_array($curl,
 		array(
 			CURLOPT_URL => 'https://api.github.com/repos/'.$_GET['path'],
 			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_VERBOSE => true,
 			CURLOPT_TIMEOUT => '3',
-			CURLOPT_HTTPHEADER => array('User-Agent: reposs [shields.io service]')
+			CURLOPT_STDERR => $fp,
+			CURLOPT_HTTPHEADER => array('User-Agent: reposs [shields.io service]', 'Authorization: token '.getenv['GITHUB_API_KEY'])
 		)
 	);
-
 	$result = json_decode(curl_exec($curl),true);
 
 	curl_close($curl);
@@ -38,5 +41,19 @@
 	    return round(pow(1024, $base - floor($base)), $precision) . $suffixes[floor($base)];
 	}
 
-	echo file_get_contents('https://img.shields.io/badge/repo_size-'.str_replace(' ','_',formatBytes($result['size'], 1)).'-'.$color.'.svg'.$style);
+	$curl = curl_init();
+
+	curl_setopt_array($curl,
+		array(
+			CURLOPT_URL => 'https://img.shields.io/badge/repo_size-'.str_replace(' ','_',formatBytes($result['size'], 1)).'-'.$color.'.svg'.$style,
+			CURLOPT_RETURNTRANSFER => 1,
+			CURLOPT_FOLLOWLOCATION => true,
+			CURLOPT_VERBOSE => true,
+			CURLOPT_TIMEOUT => '3',
+			CURLOPT_STDERR => $fp,
+			CURLOPT_HTTPHEADER => array('User-Agent: reposs [shields.io service]')
+		)
+	);
+	echo curl_exec($curl);
+
 ?>
